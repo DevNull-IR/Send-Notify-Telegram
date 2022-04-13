ce7d213d9c01a787d701d7e5328e9a3dec262ad7
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-04-13 16:55:28 +0430

    Update telegram_bot_notif.php

--- migrations/telegram_bot_notif.php
+++ migrations/telegram_bot_notif.php
@@ -13,13 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('bot_', function (Blueprint $table) {
+        Schema::create('bot_notif', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('user_id')->unique();
+            $table->integer('user_web')->unique();
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bot_notif');
     }
 };
