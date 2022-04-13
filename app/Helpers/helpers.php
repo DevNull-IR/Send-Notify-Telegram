34be2f09d68f62b9242b6592d9cfb56a29c97e08
Ali <97373457+DevNull-IR@users.noreply.github.com>
2022-04-13 16:45:23 +0430

    Update helpers.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -2,7 +2,9 @@
 
 use \App\Models\User;
 if (! function_exists('sendNotif')){
-  function sendNotif(string $data = "NAN"){
-    
+  function sendNotif(string $data = "NAN", $Is_Channel = false){
+    $data = str_replace('
+',PHP_EOL,$data);
+    request('url',env('Bot_Api_Token'));
   }
 }
